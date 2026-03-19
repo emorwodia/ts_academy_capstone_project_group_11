72a442eb4b061d73848d2f18a94d7255739c8ab6
dawodutemiloluwa708-code <dawodutemiloluwa708@gmail.com>
2026-03-20 00:18:09 +0100

    PlanetCard.jsx

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -9,3 +9,51 @@ const PlanetCard = () => {
 };
 
 export default PlanetCard;
+
+[
+    {
+        "planet": "Mercury",
+        "distanceFromSun": 57.9,
+        "image": "../images/mercury.jpg"
+    },
+    {
+        "planet": "Venus",
+        "distanceFromSun": 108.2,
+        "image": "../images/venus.webp"
+    },
+    {
+        "planet": "Earth",
+        "distanceFromSun": 149.6,
+        "image": "../images/earth.jpg"
+    },
+    {
+        "planet": "Mars",
+        "distanceFromSun": 227.9,
+        "image": "../images/mars.webp"
+    },
+    {
+        "planet": "Jupiter",
+        "distanceFromSun": 778.6,
+        "image": "../images/jupiter.webp"
+    },
+    {
+        "planet": "Saturn",
+        "distanceFromSun": 1433.5,
+        "image": "../images/saturn.webp"
+    },
+    {
+        "planet": "Uranus",
+        "distanceFromSun": 2872.5,
+        "image": "../images/uranus.webp"
+    },
+    {
+        "planet": "Neptune",
+        "distanceFromSun": 4495.1,
+        "image": "../images/neptune.webp"
+    },
+    {
+        "planet": "Pluto",
+        "distanceFromSun": 5906.4,
+        "image": "../images/pluto.webp"
+    }
+]
