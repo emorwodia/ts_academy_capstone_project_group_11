686f67c39d09911314635f543dc12d3120e6f496
dustan1 <dustanc591@gmail.com>
2026-03-25 12:41:38 +0100

    worked on the video session and made it responsive

--- src/components/PlanetGallery.jsx
+++ src/components/PlanetGallery.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import PlanetCard from './PlanetCard';
+// import PlanetCard from './PlanetCard';
 
 const PlanetGallery = () => {
   return (
     <section className="planet-gallery">
       <h2>Visualizing the Differences Between Planets</h2>
       <div className="gallery-grid">
-         <PlanetCard /> 
+         {/* <PlanetCard />  */}
       </div>
     </section>
   );
--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import './VideoSection.css';
 const VideoSection = () => {
   return (
     <section className="video-section">
-      VideoSection
+      <div className='container'>
+          <div>
+             <video loop muted autoPlay controls src="./src/assets/video.mp4"></video>
+           
+          </div>
+          <div>
+            <h2 className='planetary'>How Planetary Data <br className='mobile' />  Helps Us<br className='hide-on-mobile'/> Understand <br className='mobile'/>  Space</h2>
+            <p className='p-1'>Planetary science goes beyond images.<br className='mobile' /> Comparing <span>mass</span>, <span>diameter</span>,<br className='hide-on-mobile'/><span>gravity</span>,<br className='mobile' /> and <span>density</span>, we gain insight into how <br className='mobile' /> planets form, behave,<br className='hide-on-mobile'/> and interact <br className='mobile' /> within the solar system.</p>
+          </div>
+      </div>
     </section>
   );
 };
