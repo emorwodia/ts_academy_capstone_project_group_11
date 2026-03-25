2aa0f9cb0f7b8721fa239cf99ac55a0f57299343
Abdulsalam <habeebabdulsalam86@gmail.com>
2026-03-25 08:17:48 +0100

    removed the navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-
-import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import VideoSection from './components/VideoSection'
-import PlanetGallery from './components/PlanetGallery'
-import FactsSection from './components/FactsSection'
-import ContactForm from './components/ContactForm'
-import Footer from './components/Footer'
-import './App.css'
+import React from "react";
+import Hero from "./components/Hero";
+import VideoSection from "./components/VideoSection";
+import PlanetGallery from "./components/PlanetGallery";
+import FactsSection from "./components/FactsSection";
+import ContactForm from "./components/ContactForm";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <main className="app-container">
-      <Navbar />
       <Hero />
       <VideoSection />
       <PlanetGallery />
@@ -20,7 +17,7 @@ function App() {
       <ContactForm />
       <Footer />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
